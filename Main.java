f0e9fe89d9badae0ba58091d5aa843b15f4070ca
Komal-Chinnu <komalpawar2k13@gmail.com>
2025-05-06 11:28:11 +0530

    Saved

---
+++ Main.java
@@ -0,0 +1,15 @@
+package Program2;
+
+// File: MainApp.java
+import java.util.Date;
+
+public class Main {
+    public static void main(String[] args) {
+       
+        Date dob = new Date(103, 8, 13);  // 2003, September, 13
+        
+        Employee emp = new Employee("Komal", dob, 101, 50000.0);
+
+        emp.displayEmployeeDetails();
+    }
+}
---
+++ Person.java
@@ -0,0 +1,32 @@
+package Program2;
+
+
+import java.util.Date;
+
+public class Person {
+    String name;
+    Date dateOfBirth;
+
+    
+    public Person(String name, Date dateOfBirth) {
+        this.name = name;
+        this.dateOfBirth = dateOfBirth;
+    }
+
+   
+    public void displayName() {
+        System.out.println("Name: " + name);
+    }
+
+    
+    public void displayAge() {
+        Date today = new Date(); 
+
+        int birthYear = 1900 + dateOfBirth.getYear();
+        int currentYear = 1900 + today.getYear();
+
+        int age = currentYear - birthYear;
+        System.out.println("Age: " + age + " years");
+    }
+}
+
